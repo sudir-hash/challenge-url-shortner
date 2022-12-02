7a5c871550b98007d3022f4f9ed07509494876c2
Sudhir <sudirsha02@example.com>
2022-12-02 19:09:18 +0530

    challenge complete

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const {search,addUrl,searchUrl} = require('./utils')
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,6 +21,49 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+app.post('/api/shorturl',(req,res)=>{
+  let url = req.body.url;
+  console.log(`Url : ${url}`);
+  let result  = {
+    original_url  :url
+  }
+
+  let searchResult  = search(url);
+
+  if(searchResult.availability===true){
+    result  = {
+      ...result,
+      short_url : searchResult.short_url
+    }
+  }else{
+    let short_url = addUrl(url);
+    result  = {
+      ...result,
+      short_url : short_url
+    }
+  }
+
+
+  res.json(result);
+})
+
+app.get('/api/shorturl/:short_url',(req,res)=>{
+  let short_url = req.params.short_url;
+  short_url     = Number(short_url);
+  let searchResult  = searchUrl(short_url)
+  console.log(typeof searchResult);
+  if(typeof searchResult!=="undefined")res.redirect(`${searchResult}`);
+  console.log("Invalid Url",searchResult);
+  result= { error: 'invalid url' };
+  
+  
+  res.json(result);
+  
+})
+
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+  console.log(`Url : http://localhost:${port}`);
 });
---
+++ utils.js
@@ -0,0 +1,54 @@
+const Urls  =   require('./Urls.json');
+const fs    =   require('fs');
+
+
+
+const search    = url =>{
+    url=url.trim();
+    if(Urls.url){
+        console.log(Urls.url);
+        return {
+                availability    :   true,
+                original_url    :   url,
+                short_url       :   Urls.url
+            };
+    }else{
+        return {
+            availability    :   false,
+        };
+    }
+}
+
+
+const searchUrl = url=>{
+    url =   Number(url);
+    let shortUrls      =    Object.keys(Urls);
+    let originalUrl    =    shortUrls.find(key => Urls[key] === url);
+    console.log(`url is ${originalUrl}`);
+    return originalUrl;
+}
+
+
+const addUrl   =  url =>{
+    url=url.trim();
+    let length  =   Object.keys(Urls).length;
+    Urls[url]    =   length+1;
+    let urlObject   =   JSON.stringify(Urls);
+    try {
+        fs.writeFile("./Urls.json",urlObject,(err)=>{
+            if(err){
+                throw Error("Error While Writting to File")
+            }
+        })
+    } catch (error) {
+        console.log("Error While Writting to File");
+        console.log(error);
+    }
+    return  {
+        short_url   :   length+1
+    }
+}
+
+
+module.exports  =   {addUrl,search,searchUrl};
+
